9505327620cc6b938f79c599bfc8b2d22e5abe0f
12UE <1258432472@qq.com>
2024-03-25 09:54:49 +0800

    添加项目文件。

---
+++ SystemRoutineFinder/SystemRoutineFinder.c
@@ -0,0 +1,12 @@
+
+#include<ntifs.h>
+#include"header.h"
+//导出GetZwRoutineAddressByName 函数
+extern PVOID GetExportedRoutineAddressByName(char* RoutineName);
+PVOID GetExportedRoutineAddressByName(char* RoutineName) {
+	//将RoutineName转为PANSI_STRING类型
+	ANSI_STRING RoutineNameAnsi;
+	RtlInitAnsiString(&RoutineNameAnsi, RoutineName);
+	return (PVOID)GetZwRoutineAddressByName(&RoutineNameAnsi);
+}
+
